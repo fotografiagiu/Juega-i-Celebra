cbae88a1d7af4b213f7eea0cb8b3014fb9ca59d0
fotografiagiu <fotografiapgiu@gmail.com>
2026-01-21 17:20:40 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -66,7 +66,7 @@ export default function App() {
           </section>
         )}
 
-        {false && (
+        {true && (
           <section id="contacto" className="py-20">
             <Contact />
           </section>
