91e218644653af3065d7abcc6db21925797106f2
fotografiagiu <fotografiapgiu@gmail.com>
2026-01-29 09:35:22 +0100

    Update Gallery.tsx

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,11 +1,6 @@
-// components/Gallery.tsx
-import React from "react";
-import type { Lang } from "../src/i18n";
+
 import GalleryFan from "./GalleryFan";
 
-type Props = { lang: Lang };
-
-export default function Gallery({ lang }: Props) {
-  return <GalleryFan lang={lang} />;
+export default function Gallery() {
+  return <GalleryFan />;
 }
-
